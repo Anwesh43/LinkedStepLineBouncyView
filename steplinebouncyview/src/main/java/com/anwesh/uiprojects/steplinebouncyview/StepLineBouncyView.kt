2333a4dfc994221ef83a47faec7ab84804c873db
Anwesh43 <anweshthecool0@gmail.com>
2019-12-24 12:47:51 +0530

    adding SLBNode class

--- steplinebouncyview/src/main/java/com/anwesh/uiprojects/steplinebouncyview/StepLineBouncyView.kt
+++ steplinebouncyview/src/main/java/com/anwesh/uiprojects/steplinebouncyview/StepLineBouncyView.kt
@@ -161,4 +161,28 @@ class StepLineBouncyView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class StepLineBouncy(var i : Int) {
+
+        private val root : SLBNode = SLBNode(0)
+        private var curr : SLBNode = root
+        private var dir : Int = 1
+
+        fun  draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
