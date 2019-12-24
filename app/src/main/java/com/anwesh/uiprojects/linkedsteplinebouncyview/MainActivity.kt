662917c67d716fe409843b1af7ac726ccb7cc28b
Anwesh43 <anweshthecool0@gmail.com>
2019-12-24 12:53:32 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedsteplinebouncyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsteplinebouncyview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedsteplinebouncyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.steplinebouncyview.StepLineBouncyView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StepLineBouncyView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
