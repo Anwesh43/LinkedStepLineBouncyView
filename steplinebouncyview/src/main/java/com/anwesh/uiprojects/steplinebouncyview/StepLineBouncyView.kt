f8feb27051bd7a092d7f40ae0688998eafcff859
Anwesh43 <anweshthecool0@gmail.com>
2019-12-24 12:37:29 +0530

    adding StepLineBouncyView class

--- steplinebouncyview/src/main/java/com/anwesh/uiprojects/steplinebouncyview/StepLineBouncyView.kt
+++ steplinebouncyview/src/main/java/com/anwesh/uiprojects/steplinebouncyview/StepLineBouncyView.kt
@@ -53,3 +53,21 @@ fun Canvas.drawSLBNode(i : Int, scale : Float, paint : Paint) {
     drawStepLine(i, scale, h, gap, paint)
     restore()
 }
+
+class StepLineBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
