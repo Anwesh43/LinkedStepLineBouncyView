ae224bab7720163d32d5f9d2f49e101d1799bfce
Anwesh43 <anweshthecool0@gmail.com>
2019-12-24 12:19:31 +0530

    adding constants and libraries

--- steplinebouncyview/src/main/java/com/anwesh/uiprojects/steplinebouncyview/StepLineBouncyView.kt
+++ steplinebouncyview/src/main/java/com/anwesh/uiprojects/steplinebouncyview/StepLineBouncyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.steplinebouncyview
 /**
  * Created by anweshmishra on 24/12/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val strokeFactor : Int = 90
+val offset : Float = 0.1f
+val scGap : Float = 0.005f
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
